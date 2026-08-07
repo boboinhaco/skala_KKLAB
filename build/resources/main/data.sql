e1575e3ec3d7643b1c4f03cfbae64c83eb84adc3
최인서 <inseo6224@naver.com>
2026-08-07 16:51:11 +0900

    전체 고객 목록 조회 api 구현

---
+++ build/resources/main/data.sql
@@ -0,0 +1,6 @@
+-- 초기 상품 시드 데이터
+INSERT INTO product (product_name, product_price) VALUES ('무선마우스', 15000);
+INSERT INTO product (product_name, product_price) VALUES ('블루투스키보드', 29000);
+INSERT INTO product (product_name, product_price) VALUES ('USB허브', 39000);
+INSERT INTO product (product_name, product_price) VALUES ('노트북거치대', 25000);
+INSERT INTO product (product_name, product_price) VALUES ('웹캠', 55000);
--- src/main/java/com/sk/skala/shopapi/service/CustomerService.java
+++ src/main/java/com/sk/skala/shopapi/service/CustomerService.java
@@ -2,6 +2,11 @@ package com.sk.skala.shopapi.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import com.sk.skala.shopapi.common.PagedList;
 
 import com.sk.skala.shopapi.common.Response;
 import com.sk.skala.shopapi.common.SessionHandler;
@@ -26,10 +31,17 @@ public class CustomerService {
 
 	// 전체 고객 목록 조회 (페이지 단위)
 	public Response getAllCustomers(int offset, int count) {
-		// TODO 1. PageRequest.of(offset, count)로 Pageable 생성
-		// TODO 2. customerRepository.findAll(pageable) 조회
-		// TODO 3. PagedList로 변환 후 Response.success 반환
-		throw new UnsupportedOperationException("TODO: getAllCustomers 구현");
+		Pageable pageable = PageRequest.of(offset, count);
+		Page<Customer> page = customerRepository.findAll(pageable);
+		List<Customer> customers = page.getContent(); // 비밀번호 null로 세팅
+
+		// 응답에 비밀번호가 노출되지 않도록 제거
+    	for (Customer c : customers) {
+        	c.setCustomerPassword(null);
+    	}
+
+		PagedList pagedList = new PagedList(page.getTotalElements(), offset, count, customers);
+		return Response.success(pagedList);
 	}
 
 	// 단일 고객 및 주문 상품 목록 조회
